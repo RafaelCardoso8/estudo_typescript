ff13264ded6afa084a92a7d171ab1ba94d726139
Rafael Cardoso <faelcardoso8@gmail.com>
2024-12-29 17:26:06 -0300

    feat

--- intro/index.js
+++ intro/index.js
@@ -3,3 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var texto = " Minhas Saudações mundo!! ";
 console.log(texto);
 exports.default = texto;
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(2, 5));
--- intro/index.ts
+++ intro/index.ts
@@ -3,3 +3,10 @@ let texto : string = " Minhas Saudações mundo!! "
 console.log(texto)
 
 export default texto
+
+
+function soma(a: number, b: number){
+    return a + b
+}
+
+console.log(soma(2, 5))
